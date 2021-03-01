d3c9e4911d0d13aa3388297b620d83bace93a26d
Kanika <kanika291993@gmail.com>
2021-03-01 13:06:07 +0530

    Second SignUp Button

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,9 @@
       <v-btn text rounded to="/home">Home</v-btn>
       <v-btn text rounded to="/about">About</v-btn>
       <v-btn text rounded to="/login">Login</v-btn>
+      <v-btn text rounded to="/login">SignUp</v-btn>      
       <v-btn text rounded to="/login">SignUp</v-btn>
+
     </v-app-bar>
     <v-content> 
       <router-view ></router-view>   
